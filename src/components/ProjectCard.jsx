36d06152dfccfc81c2feaa6f05ec357305f300fa
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-23 15:52:45 -0700

    fixed small errors on project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,12 +4,12 @@ import Arrow from '../../public/images/arrow.svg'
 
 export default function ProjectCard({ name, desc, image, link }) {
   return (
-    <div id='project-card' className=' gradient-border text-white mt-10 '>
+    <div id='project-card' className=' gradient-border text-white  '>
 
-      <div className='ml-5 mt-5 flex flex-col gap-4'>
-        <h1 className='text-4xl font-bold'>{name}</h1>
-        <p className='text-xl'> {desc}</p>
-        <a href={link} target='_blank' className='proj-card-btn flex justify-center items-center gap-7 mb-10'> View project <img src={Arrow} className='max-w-[35px]' />  </a>
+      <div className=' flex flex-col justify-center '>
+        <h1 className='text-4xl font-bold mb-2'>{name}</h1>
+        <p className='text-xl mb-7'> {desc}</p>
+        <a href={link} target='_blank' className='proj-card-btn flex justify-center items-center gap-7 mb-28'> View project <img src={Arrow} className='max-w-[35px]' />  </a>
 
       </div>
 
