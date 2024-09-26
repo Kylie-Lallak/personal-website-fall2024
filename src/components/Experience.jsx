f82286554c58c2263e53e9bcae9d5610d00c160d
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-26 14:40:55 -0700

    inital experince info text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/Experience.css'
 import LinkedIn from '../../public/images/linkedin.svg'
+import Neuro from '../../public/images/neruo.png'
 
 export default function Experience() {
     return (
@@ -19,8 +20,60 @@ export default function Experience() {
             </div>
 
 
+            <div className='w-[40%] ml-10'>
+                <div className='flex items-center gap-4'>
+                    <img src={Neuro} className='w-[70px]' />
+                    <h1 className='text-white text-4xl font-bold '>Neurotech@Davis</h1>
+
+                </div>
+                <div className='flex items-center justify-between bg-slate-800'> <h2 className='text-white' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
+
+                <ul className='ex-ul ml-10'>
+                    <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
+                    <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
+                    <li>Website coming soon 😊</li>
+                </ul>
+
+                <div className='flex items-center justify-between bg-slate-800'> <h2 className='text-white' >Project Member</h2> <h3 className='text-gray-600'> Oct 2023 - June 2024</h3> </div>
+
+                <ul className='ex-ul ml-10'>
+                    <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
+                    <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
+                    <li>Website coming soon 😊</li>
+                </ul>
+
+            </div>
 
 
-        </div>
+
+            <div className='w-[40%] ml-10'>
+                <div className='flex items-center gap-4'>
+                    <img src={Neuro} className='w-[70px]' />
+                    <h1 className='text-white text-4xl font-bold '>Neurotech@Davis</h1>
+
+                </div>
+                <div className='flex items-center justify-between bg-slate-800'> <h2 className='text-white' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
+
+                <ul className='ex-ul ml-10'>
+                    <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
+                    <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
+                    <li>Website coming soon 😊</li>
+                </ul>
+
+                <div className='flex items-center justify-between bg-slate-800'> <h2 className='text-white' >Project Member</h2> <h3 className='text-gray-600'> Oct 2023 - June 2024</h3> </div>
+
+                <ul className='ex-ul ml-10'>
+                    <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
+                    <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
+                    <li>Website coming soon 😊</li>
+                </ul>
+
+            </div>
+
+
+
+
+
+        </div >
     )
 }
