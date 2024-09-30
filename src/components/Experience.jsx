2e3ccab5958079b1525326b193f0082360e647af
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-29 18:00:04 -0700

    added experince images

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,10 +4,14 @@ import LinkedIn from '../../public/images/linkedin.svg'
 import Neuro from '../../public/images/neruo.png'
 import D3C from '../../public/images/d3c.png'
 import IMG from '../../public/images/about.png'
+import Neuroweb from '../../public/images/neuroweb.png'
+import Bci from '../../public/images/bci.png'
+import D3cImg from '../../public/images/d3cimg.png'
+
 
 export default function Experience() {
     return (
-        <div id='ex-sect' className='mt-16'>
+        <div id='ex-sect' className='mt-16 '>
 
             <div className='mb-20'>
                 <h1 className='font-bold text-center text-white text-5xl md:text-7xl gradient-text'>Experience</h1>
@@ -23,7 +27,7 @@ export default function Experience() {
 
 
 
-            <div id='neuro-ex'>
+            <div id='neuro-ex' className='mb-14'>
                 <div className=' flex flex-col items-center mx-auto gap-10 w-[80%]  md:flex-row '>
 
                     <div className='md:w-[50%]'>
@@ -47,7 +51,7 @@ export default function Experience() {
 
                     </div>
 
-                    <img src={IMG} className='md:w-[50%] md:ml-14' />
+                    <img src={Neuroweb} className='md:w-[35%] md:ml-14 ' />
 
 
 
@@ -72,7 +76,7 @@ export default function Experience() {
 
                     </div>
 
-                    <img src={IMG} className='md:w-[50%] md:ml-14' />
+                    <img src={Bci} className='md:w-[35%] md:ml-14 mt-5' />
                 </div>
             </div>
 
@@ -83,8 +87,8 @@ export default function Experience() {
                     <div className='md:w-[50%]'>
 
                         <div className='flex items-center gap-4'>
-                            <img src={Neuro} className='w-[70px]' />
-                            <h1 className='text-white text-4xl font-bold '>Neurotech@Davis</h1>
+                            <img src={D3C} className='w-[70px]' />
+                            <h1 className='text-white text-4xl font-bold '>SafeHer</h1>
 
                         </div>
                         <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
@@ -101,33 +105,13 @@ export default function Experience() {
 
                     </div>
 
-                    <img src={IMG} className='md:w-[50%] md:ml-14' />
+                    <img src={D3cImg} className='md:w-[35%] md:ml-14 mt-5' />
 
 
 
                 </div>
 
-                <div className=' flex flex-col items-center mx-auto gap-10 w-[80%]  md:flex-row '>
 
-                    <div className='md:w-[50%]'>
-
-
-                        <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
-
-                        <ul className='ex-ul ml-10'>
-                            <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
-                            <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
-                            <li>Website coming soon 😊</li>
-                        </ul>
-
-
-
-
-
-                    </div>
-
-                    <img src={IMG} className='md:w-[50%] md:ml-14' />
-                </div>
             </div>
 
 
