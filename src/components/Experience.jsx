907b072f6602bde38b4e927a5be7cfb1989c544e
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-30 17:49:51 -0700

    added experince section deatails

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -40,9 +40,9 @@ export default function Experience() {
                         <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
 
                         <ul className='ex-ul ml-10'>
-                            <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
+                            <li>Led a cross-functional team of 2 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
                             <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
-                            <li>Website coming soon 😊</li>
+                            <a href='https://neurotechdavis.com' target='_blank'>  <li className='web-link'>Vist the website</li> </a>
                         </ul>
 
 
@@ -51,7 +51,7 @@ export default function Experience() {
 
                     </div>
 
-                    <img src={Neuroweb} className='md:w-[35%] md:ml-14 ' />
+                    <img src={Neuroweb} className='md:w-[35%] md:ml-14 md:h-[50%] ' />
 
 
 
@@ -62,12 +62,12 @@ export default function Experience() {
                     <div className='md:w-[50%]'>
 
 
-                        <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
+                        <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Member</h2> <h3 className='text-gray-600'> Oct 2023 - June 2024</h3> </div>
 
                         <ul className='ex-ul ml-10'>
-                            <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
-                            <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
-                            <li>Website coming soon 😊</li>
+                            <li>Contributed to a cross-functional team developing a brain-computer interface (BCI) for detecting eye blinks through EEG signals</li>
+                            <li>Collected EMG and EEG data from electrodes placed on the frontal cortex and near the eyes, then implemented a real-time machine learning algorithm using a random forest classifier, achieving 86% accuracy in distinguishing intentional and unintentional blinks</li>
+                            <li>Presented at the California 2024 NeurotechX Conference, demonstrating the potential of neural signal-controlled devices, such as prosthetic limbs, and highlighting advancements in accessibility and assistive technology</li>
                         </ul>
 
 
@@ -80,8 +80,9 @@ export default function Experience() {
                 </div>
             </div>
 
+            <div className=" ml-16 mr-16 mt-28 h-px bg-gray-700 my-4 sm:ml-24 sm:mr-24"></div>
 
-            <div id='neuro-ex'>
+            <div id='d3c-ex'>
                 <div className=' flex flex-col items-center mx-auto gap-10 w-[80%]  md:flex-row '>
 
                     <div className='md:w-[50%]'>
@@ -91,12 +92,13 @@ export default function Experience() {
                             <h1 className='text-white text-4xl font-bold '>SafeHer</h1>
 
                         </div>
-                        <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> June 2024 - Present</h3> </div>
+                        <div className='flex items-center justify-between mt-4'> <h2 className='font-bold gradient-text-blue text-xl' >Project Manger</h2> <h3 className='text-gray-600'> April 2024 - Present</h3> </div>
 
                         <ul className='ex-ul ml-10'>
-                            <li>Led a cross-functional team of 3 developers and 1 designer in the development, design, and deployment of the organization's website. Successfully coordinated efforts to meet tight deadlines, ensuring a seamless user experience for over 100 members.</li>
-                            <li>Devloped using React.js Javascript, CSS, Tailwind CSS</li>
-                            <li>Website coming soon 😊</li>
+                            <li>Contributed to a Website called SafeHer, focused on promoting educational resources and spreading awareness about women's issues, making key information and resources more accessible</li>
+                            <li>Worked on developing the resource and data visualization components of the website using React, HTML, CSS, and JavaScript</li>
+                            <li>Collaborated effectively with a cross-functional team of 5 developers and 2 designers, contributing to the development of key features and ensuring cohesive design and functionality throughout the project</li>
+                            <li>Website currently under maintance ⚒️</li>
                         </ul>
 
 
